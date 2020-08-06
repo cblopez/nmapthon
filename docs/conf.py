b2bb1c0177d1a339d7636f5b2a04c4102a20f311
Christian Barral López <christianbarrallopez@Christians-MacBook-Pro.local>
2020-08-06 14:13:10 +0200

    Changed theme configuration

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -31,8 +32,7 @@ release = '1.5.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme'
 ]
 
 
@@ -50,9 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'msmb_theme'
-import msmb_theme
-html_theme_path = [msmb_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
